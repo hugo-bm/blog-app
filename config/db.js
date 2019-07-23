43322b656c8ecb79d9ce4a9c77731d260685a8cb
Your Name <you@example.com>
2019-07-23 10:59:36 -0300

    f

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == 'production') {
-    module.exports = {mongoURI: "mongodb://hugo_blog_app:asdfghjkl@cluster0-dm37c.mongodb.net/test?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://hugo_blog_app:asdfghjkl@cluster0-dm37c.mongodb.net/test?retryWrites=true&w=majority"}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/blog_appDB'}
 }
